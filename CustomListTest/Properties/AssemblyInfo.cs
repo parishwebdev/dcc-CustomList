5bc14963b9eb4a951cf1e1d0951e09c231366686
parishwebdev <parishwebdev@gmail.com>
2018-06-05 19:42:40 -0500

    added initial files/Unit Testing

---
+++ CustomListTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomListTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3f643640-2e9d-44b9-9dbd-d6735d4776ef")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DCC-CustomList/CustomList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCC_CustomList
+{
+    public class CustomList<T>
+    {
+
+        public void Add()
+        {
+
+        }
+        public void Remove()
+        {
+
+        }
+
+        //Indexer
+
+        //-----------------------------
+        //Come to Later
+        public void Zip()
+        {
+
+        }
+
+        /*
+          Overriding + and -
+         */
+
+    }
+}
